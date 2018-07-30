43e317d83973683740c6b7354cd8391be946f04f
Eric Choy <echoy20@gmail.com>
2018-07-30 17:35:48 -0400

    Add populate, [], []=, move_piece, valid_pos?, and add_piece to board.rb

--- Chess/board.rb
+++ Chess/board.rb
@@ -1,9 +1,13 @@
+require_relative 'piece.rb'
+require 'byebug'
+
 class Board
-  attr_reader :rows
+  attr_accessor :rows
 
   def initialize
     @rows = Array.new(8){ Array.new(8)}
-    @sentinel = NullPiece.new
+    @white_rook = Piece.new(:white, @rows, [0,4] )
+    # @sentinel = NullPiece.new()
   end
 
   def [](pos)
@@ -13,9 +17,58 @@ class Board
 
   def []=(pos, val)
     x,y = pos
-    rows[x][y] = val
-  end 
+    rows[pos] = val
+  end
+
+  def populate
+    @rows.each_index do |row|
+      @rows[row].each_index do |tile|
+        @rows[row][tile] = NullPiece.new([row, tile])
+
+      end
+    end
+    add_piece(@white_rook, [0, 4])
+  end
+
+  def move_piece(start_pos, end_pos)
+    if rows[start_pos] == NullPiece
+      raise "Invalid start position"
+    elsif valid_pos?(end_pos) == false
+      raise "Invalid end position"
+    end
+
+    rows[end_pos] = rows[start_pos]
+    rows[start_pos] = NullPiece
+
+  end
+
+  def valid_pos?(pos)
+    pos[0] < 8  && pos[1] < 8 && pos[0] >= 0 && pos[1] >= 0
+  end
+
+  def add_piece(piece, pos)
+    @rows[pos] = piece
+  end
+
+  def checkmate?(color)
+  end
+
+  def in_check?(color)
+  end
+
+  def find_king(color)
+  end
+
+  def pieces
+  end
+
+  def dup
+  end
+
+  def move_piece!(color, start_pos, end_pos)
+  end
 
   private
   attr_reader :sentinel
+
 end
