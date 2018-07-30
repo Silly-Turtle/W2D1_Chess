03f915bf21fc0697e718d8b3132b435d8a2f10c6
Eric Choy <echoy20@gmail.com>
2018-07-30 17:36:45 -0400

    Create rook and NullPiece classes

--- Chess/piece.rb
+++ Chess/piece.rb
@@ -1,8 +1,44 @@
 class Piece
+  def initialize(color, board, pos)
+    @color = color
+    @board = board
+    @pos = pos
+  end
+
+  def to_s
+  end
+
+  def empty?
+  end
+
+  def valid_moves
+  end
+
+  def pos=(val)
+  end
+
+  def symbol
+  end
+
+  def move_into_check?(end_pos)
+  end
 
 end
 
+class Rook < Piece
+  def symbol
+    :rook
+  end
+
+  protected
+
+  def move_dirs
+  end
+
+end
 
 class NullPiece < Piece
-
-end 
+  def initialize(pos)
+    @pos = pos 
+  end
+end
